102a2daf302506fc13c3b55e07c9f6d0838acce8
Matt <matt.b.wilder1@gmail.com>
2022-08-07 10:05:06 -0600

    update vault contract

---
+++ src/Vault.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+error TransferFailed();
+
+contract Vault {
+    mapping(address => uint256) public s_balances;
+
+    function stake(uint256 amount, address token) external returns (bool) {
+        s_balances[msg.sender] += amount;
+        bool success = IERC20(token).transferFrom(
+            msg.sender,
+            address(this),
+            amount
+        );
+        if (!success) revert TransferFailed();
+        return success;
+    }
+}
