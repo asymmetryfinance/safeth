8d8fe69cc4e528157aabfb6a0574ff6be2e6a46b
Matt <matt.b.wilder1@gmail.com>
2022-08-10 06:54:02 -0600

    add shares math to vault.sol deposit fn

--- src/Vault.sol
+++ src/Vault.sol
@@ -5,9 +5,11 @@ pragma solidity ^0.8.13;
 // import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 /// @title Golden Ratio vault
-/// @notice Allows users to deposit ETH/CVX into autocompounding strategy contracts (e.g. {StrategyCRV}).
+/// @notice Allows users to deposit ETH/CVX into autocompounding strategy contracts (e.g. {CrvDepositor}).
 contract Vault {
     event Deposit(uint256 value);
+    // event Deposit(address indexed from, address indexed to, uint256 value);
+    // event Withdrawal(address indexed withdrawer, address indexed to, uint256 wantAmount);
 
     address[] private funders;
     uint256 public assetsDeposited;
@@ -15,7 +17,7 @@ contract Vault {
 
     /// @notice Allows users to deposit `token`. Contracts can't call this function
     /// @param _amount The amount to deposit
-    function deposit(uint256 _amount) public payable {
+    function deposit(uint256 _amount) external {
         uint256 amount = _amount;
         require(amount == 48 ether, "INVALID_AMOUNT");
 
@@ -23,8 +25,22 @@ contract Vault {
         assetsDeposited += amount;
         funders.push(msg.sender);
 
+        // managing shares of tokens: token maths
+        /*
+        a = amount
+        B = balance of token before deposit
+        T = total supply
+        s = shares to mint
+
+        (T + s) / T = (a + B) / B 
+
+        s = aT / B
+        */
+
         emit Deposit(amount);
+        // emit Deposit(msg.sender, _to, amountAfterFee);
     }
 
     // withdraw
+    // emit Withdrawal(msg.sender, _to, backingTokens);
 }
