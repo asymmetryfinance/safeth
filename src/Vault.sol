64dc87ef757c872464f3d99314b18c91c98cccd0
Matt <matt.b.wilder1@gmail.com>
2022-08-16 12:49:17 -0600

    import SafeERC20Upgradeable.sol + initializer from openzeppelin

--- src/Vault.sol
+++ src/Vault.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.13;
 
 // imports
 // import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeable.sol";
+import "@openzeppelin-upgradeable/contracts/token/ERC20/utils/SafeERC20Upgradeable.sol";
 
 /// @title Golden Ratio vault
 /// @notice Allows users to deposit ETH/CVX into autocompounding strategy contracts (e.g. {CrvDepositor}).
