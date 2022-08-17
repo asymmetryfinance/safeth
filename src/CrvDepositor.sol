8bd1ded63f90301573c9fa5adfbafd1baf48ffee
Matt <matt.b.wilder1@gmail.com>
2022-08-16 19:29:24 -0600

    update crv interfaces

--- src/CrvDepositor.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
-
-import "./interfaces/ICurve.sol";
-import "./interfaces/I3CRVZap.sol";
-import "./interfaces/IBooster.sol";
-import "./interfaces/IBaseRewardPool.sol";
-
-contract CrvDepositor {}
---
+++ src/interfaces/ICurveFi_Minter.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+/**
+ * @dev Interface for Curve.Fi CRV minter contract.
+ * @dev See original implementation in official repository:
+ * https://github.com/curvefi/curve-dao-contracts/blob/master/contracts/Minter.vy
+ */
+interface ICurveFi_Minter {
+    function mint(address gauge_addr) external;
+
+    function mint_for(address gauge_addr, address _for) external;
+
+    function minted(address _for, address gauge_addr)
+        external
+        view
+        returns (uint256);
+
+    function toggle_approve_mint(address minting_user) external;
+
+    function token() external view returns (address);
+}
