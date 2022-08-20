f3949a77f3a7a5664c891d8b8ff908e02eca13bd
Matt <matt.b.wilder1@gmail.com>
2022-08-20 12:42:03 -0600

    add IStrategy.sol

---
+++ src/interfaces/IStrategy.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+/**
+ * @dev Interface for GRStrategy.
+ * @dev See original implementation in official repository:
+ * https://github.com/curvefi/curve-contract/blob/master/contracts/pool-templates/base/SwapTemplateBase.vy
+ */
+
+interface IStrategy {
+
+}
