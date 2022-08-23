5e5234ede58aa273f812c5bf22498fc805b3b7d9
Matt <matt.b.wilder1@gmail.com>
2022-08-23 12:00:02 -0600

    update vault to 4626 standard

--- src/GRStrategy.sol
+++ src/GRStrategy.sol
@@ -1,4 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-contract GRStrategy {}
+contract GRStrategy {
+    // swap ETH for WETH
+    // Deposit WETH in Curve Pool
+    // Stake WETH for stETH
+    // Stake WETH for rETH
+    //
+}
--- test/Vault.t.sol
+++ test/Vault.t.sol
@@ -4,15 +4,4 @@ pragma solidity ^0.8.13;
 import "forge-std/Test.sol";
 import "../src/Vault.sol";
 
-contract VaultTest is Test {
-    Vault public vault;
-
-    function setUp() public {
-        vault = new Vault();
-    }
-
-    function testDeposit() public {
-        uint256 num = 69;
-        assertEq(num, 69);
-    }
-}
+contract VaultTest is Test {}
