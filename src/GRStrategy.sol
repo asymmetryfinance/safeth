ea834d29beb2046863ca9eb05956fae96a5bb83e
Matt <matt.b.wilder1@gmail.com>
2022-08-23 12:16:24 -0600

    update strat

--- src/GRStrategy.sol
+++ src/GRStrategy.sol
@@ -1,10 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
+import "./interfaces/curve/ICurve.sol";
+
+/**
+ * @title Golden Ratio Base ETH Strategy
+ * @notice This strategy autocompounds Convex rewards from the PUSD/USDC/USDT/DAI Curve pool
+ * @dev The strategy deposits 33.3% ETH in the ETH/grETH Curve pool, swaps 33.3% ETH for CVX and locks up CVX,
+ * and deposits remaining 33.3% ETH into liquid staked ETH Balancer pool
+ */
 contract GRStrategy {
-    // swap ETH for WETH
-    // Deposit WETH in Curve Pool
-    // Stake WETH for stETH
-    // Stake WETH for rETH
-    //
+
 }
