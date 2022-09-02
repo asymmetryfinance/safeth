7b6f154872664f0a307f1f07988ed0676f243a9a
Matt <matt.b.wilder1@gmail.com>
2022-09-02 11:10:03 -0600

    update vault + controller

--- src/interfaces/IERC4626.sol
+++ src/interfaces/IERC4626.sol
@@ -1,178 +1,111 @@
-// SPDX-License-Identifier: AGPL-3.0-only
-pragma solidity ^0.8.12;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {ERC20} from "solmate/tokens/ERC20.sol";
 
-interface IERC4626 is IERC20 {
-    /*//////////////////////////////////////////////////////////////
-                                 EVENTS
+abstract contract IERC4626 is ERC20 {
+    /*///////////////////////////////////////////////////////////////
+                                Events
     //////////////////////////////////////////////////////////////*/
 
     event Deposit(
-        address indexed caller,
-        address indexed owner,
-        uint256 assets,
+        address indexed from,
+        address indexed to,
+        uint256 amount,
         uint256 shares
     );
-
     event Withdraw(
-        address indexed caller,
-        address indexed receiver,
-        address indexed owner,
-        uint256 assets,
+        address indexed from,
+        address indexed to,
+        uint256 amount,
         uint256 shares
     );
 
-    /*//////////////////////////////////////////////////////////////
-                               IMMUTABLES
+    /*///////////////////////////////////////////////////////////////
+                            Mutable Functions
     //////////////////////////////////////////////////////////////*/
 
-    function asset() external view returns (address);
+    function deposit(uint256 amount, address to)
+        public
+        virtual
+        returns (uint256 shares);
 
-    /*//////////////////////////////////////////////////////////////
-                        DEPOSIT/WITHDRAWAL LOGIC
-    //////////////////////////////////////////////////////////////*/
-
-    function deposit(uint256 assets, address receiver)
-        external
-        returns (uint256 shares); /* {
-        // Check for rounding error since we round down in previewDeposit.
-        require((shares = previewDeposit(assets)) != 0, "ZERO_SHARES");
-
-        // Need to transfer before minting or ERC777s could reenter.
-        asset.safeTransferFrom(msg.sender, address(this), assets);
-
-        _mint(receiver, shares);
-
-        emit Deposit(msg.sender, receiver, assets, shares);
-
-        afterDeposit(assets, shares);
-    }*/
-
-    function mint(uint256 shares, address receiver)
-        external
-        returns (uint256 assets); /* {
-        assets = previewMint(shares); // No need to check for rounding error, previewMint rounds up.
-
-        // Need to transfer before minting or ERC777s could reenter.
-        asset.safeTransferFrom(msg.sender, address(this), assets);
-
-        _mint(receiver, shares);
-
-        emit Deposit(msg.sender, receiver, assets, shares);
-
-        afterDeposit(assets, shares);
-    }*/
+    function mint(uint256 shares, address to)
+        public
+        virtual
+        returns (uint256 underlyingAmount);
 
     function withdraw(
-        uint256 assets,
-        address receiver,
-        address owner
-    ) external returns (uint256 shares); /* {
-        shares = previewWithdraw(assets); // No need to check for rounding error, previewWithdraw rounds up.
-
-        if (msg.sender != owner) {
-            uint256 allowed = allowance[owner][msg.sender]; // Saves gas for limited approvals.
-
-            if (allowed != type(uint256).max) allowance[owner][msg.sender] = allowed - shares;
-        }
-
-        beforeWithdraw(assets, shares);
-
-        _burn(owner, shares);
-
-        emit Withdraw(msg.sender, receiver, owner, assets, shares);
-
-        asset.safeTransfer(receiver, assets);
-    }*/
+        uint256 amount,
+        address to,
+        address from
+    ) public virtual returns (uint256 shares);
 
     function redeem(
         uint256 shares,
-        address receiver,
-        address owner
-    ) external returns (uint256 assets); /* {
-        if (msg.sender != owner) {
-            uint256 allowed = allowance[owner][msg.sender]; // Saves gas for limited approvals.
+        address to,
+        address from
+    ) public virtual returns (uint256 amount);
 
-            if (allowed != type(uint256).max) allowance[owner][msg.sender] = allowed - shares;
-        }
-
-        // Check for rounding error since we round down in previewRedeem.
-        require((assets = previewRedeem(shares)) != 0, "ZERO_ASSETS");
-
-        beforeWithdraw(assets, shares);
-
-        _burn(owner, shares);
-
-        emit Withdraw(msg.sender, receiver, owner, assets, shares);
-
-        asset.safeTransfer(receiver, assets);
-    }*/
-
-    /*//////////////////////////////////////////////////////////////
-                            ACCOUNTING LOGIC
+    /*///////////////////////////////////////////////////////////////
+                            View Functions
     //////////////////////////////////////////////////////////////*/
 
-    function totalAssets() external view returns (uint256);
+    function totalAssets() public view virtual returns (uint256);
 
-    function convertToShares(uint256 assets) external view returns (uint256); /* {
-        uint256 supply = totalSupply; // Saves an extra SLOAD if totalSupply is non-zero.
+    function assetsOf(address user) public view virtual returns (uint256);
 
-        return supply == 0 ? assets : assets.mulDivDown(supply, totalAssets());
-    }*/
+    function assetsPerShare() public view virtual returns (uint256);
 
-    function convertToAssets(uint256 shares) external view returns (uint256); /* {
-        uint256 supply = totalSupply; // Saves an extra SLOAD if totalSupply is non-zero.
+    function maxDeposit(address) public virtual returns (uint256);
 
-        return supply == 0 ? shares : shares.mulDivDown(totalAssets(), supply);
-    }*/
+    function maxMint(address) public virtual returns (uint256);
 
-    function previewDeposit(uint256 assets) external view returns (uint256); /* {
-        return convertToShares(assets);
-    }*/
+    function maxRedeem(address user) public view virtual returns (uint256);
 
-    function previewMint(uint256 shares) external view returns (uint256); /* {
-        uint256 supply = totalSupply; // Saves an extra SLOAD if totalSupply is non-zero.
+    function maxWithdraw(address user) public view virtual returns (uint256);
 
-        return supply == 0 ? shares : shares.mulDivUp(totalAssets(), supply);
-    }*/
+    /**
+      @notice Returns the amount of vault tokens that would be obtained if depositing a given amount of underlying tokens in a `deposit` call.
+      @param underlyingAmount the input amount of underlying tokens
+      @return shareAmount the corresponding amount of shares out from a deposit call with `underlyingAmount` in
+     */
+    function previewDeposit(uint256 underlyingAmount)
+        public
+        view
+        virtual
+        returns (uint256 shareAmount);
 
-    function previewWithdraw(uint256 assets) external view returns (uint256); /* {
-        uint256 supply = totalSupply; // Saves an extra SLOAD if totalSupply is non-zero.
+    /**
+      @notice Returns the amount of underlying tokens that would be deposited if minting a given amount of shares in a `mint` call.
+      @param shareAmount the amount of shares from a mint call.
+      @return underlyingAmount the amount of underlying tokens corresponding to the mint call
+     */
+    function previewMint(uint256 shareAmount)
+        public
+        view
+        virtual
+        returns (uint256 underlyingAmount);
 
-        return supply == 0 ? assets : assets.mulDivUp(supply, totalAssets());
-    }*/
+    /**
+      @notice Returns the amount of vault tokens that would be burned if withdrawing a given amount of underlying tokens in a `withdraw` call.
+      @param underlyingAmount the input amount of underlying tokens
+      @return shareAmount the corresponding amount of shares out from a withdraw call with `underlyingAmount` in
+     */
+    function previewWithdraw(uint256 underlyingAmount)
+        public
+        view
+        virtual
+        returns (uint256 shareAmount);
 
-    function previewRedeem(uint256 shares) external view returns (uint256); /* {
-        return convertToAssets(shares);
-    }*/
-
-    /*//////////////////////////////////////////////////////////////
-                     DEPOSIT/WITHDRAWAL LIMIT LOGIC
-    //////////////////////////////////////////////////////////////*/
-
-    function maxDeposit(address) external view returns (uint256); /* {
-        return type(uint256).max;
-    }*/
-
-    function maxMint(address) external view returns (uint256); /* {
-        return type(uint256).max;
-    }*/
-
-    function maxWithdraw(address owner) external view returns (uint256); /* {
-        return convertToAssets(balanceOf[owner]);
-    }*/
-
-    function maxRedeem(address owner) external view returns (uint256); /* {
-        return balanceOf[owner];
-    }*/
-
-    /*//////////////////////////////////////////////////////////////
-                          INTERNAL HOOKS LOGIC
-    //////////////////////////////////////////////////////////////*/
-    /*
-    function beforeWithdraw(uint256 assets, uint256 shares) internal {}
-
-    function afterDeposit(uint256 assets, uint256 shares) internal {}
-    */
+    /**
+      @notice Returns the amount of underlying tokens that would be obtained if redeeming a given amount of shares in a `redeem` call.
+      @param shareAmount the amount of shares from a redeem call.
+      @return underlyingAmount the amount of underlying tokens corresponding to the redeem call
+     */
+    function previewRedeem(uint256 shareAmount)
+        public
+        view
+        virtual
+        returns (uint256 underlyingAmount);
 }
