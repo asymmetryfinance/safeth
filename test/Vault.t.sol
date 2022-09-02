581fb49a98314a696d21ac5e720ff1dacd825544
Matt <matt.b.wilder1@gmail.com>
2022-09-02 15:18:29 -0600

    update IERC4626

--- test/Vault.t.sol
+++ test/Vault.t.sol
@@ -3,5 +3,19 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
 import "../src/Vault.sol";
+import "../src/Controller.sol";
 
-contract VaultTest is Test {}
+//import "../src/StrategyGoldenRatio.sol";
+
+contract VaultTest is Test {
+    Vault public vault;
+
+    //Controller public controller;
+
+    // Vault public vault;
+
+    function setUp() public {
+        //vault = new Vault();
+        //controller = new Controller();
+    }
+}
