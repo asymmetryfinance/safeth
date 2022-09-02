a1338d7ece25308d39736b7445114ccc5720109a
Matt <matt.b.wilder1@gmail.com>
2022-09-02 15:18:29 -0600

    update IERC4626

--- src/interfaces/IStrategy.sol
+++ src/interfaces/IStrategy.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.13;
 
 /**
- * @dev Interface for GRStrategy.
+ * @dev Interface for StrategyGoldenRatio.
  * @dev See original implementation in official repository:
  * https://github.com/projectgoldenratio/core/blob/main/src/interfaces/IStrategy.sol
  */
