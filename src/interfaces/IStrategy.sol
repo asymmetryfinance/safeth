6311c7af04fd9d76a9973b03ec39f47a7d84a0ef
Matt <matt.b.wilder1@gmail.com>
2022-09-06 07:20:30 -0600

    update controller

--- src/interfaces/IStrategy.sol
+++ src/interfaces/IStrategy.sol
@@ -1,12 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-/**
- * @dev Interface for StrategyGoldenRatio.
- * @dev See original implementation in official repository:
- * https://github.com/projectgoldenratio/core/blob/main/src/interfaces/IStrategy.sol
- */
-
 interface IStrategy {
+    function want() external view returns (address);
 
+    function deposit() external;
+
+    // NOTE: must exclude any tokens used in the yield
+    // Controller role - withdraw should return to Controller
+    function withdraw(address) external;
+
+    // Controller | Vault role - withdraw should always return to Vault
+    function withdraw(uint256) external;
+
+    // Controller | Vault role - withdraw should always return to Vault
+    function withdrawAll() external returns (uint256);
+
+    function balanceOf() external view returns (uint256);
 }
