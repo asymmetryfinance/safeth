64e392d86d205c8ded359423376c02082704af2a
Matt <matt.b.wilder1@gmail.com>
2022-09-07 13:52:00 -0600

    update stETH4626 vault

--- src/StrategyGoldenRatio.sol
+++ src/StrategyGoldenRatio.sol
@@ -1,4 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-contract StrategyGoldenRatio {}
+contract StrategyGoldenRatio {
+    address public governance;
+    address public controller;
+    address public strategist;
+
+    constructor(address _controller) {
+        governance = msg.sender;
+        strategist = msg.sender;
+        controller = _controller;
+    }
+
+    function getName() external pure returns (string memory) {
+        return "StrategyGoldenRatio";
+    }
+}
---
+++ src/interfaces/lido/IWStETH.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import {ERC20} from "solmate/tokens/ERC20.sol";
+
+abstract contract IWStETH is ERC20 {
+    function unwrap(uint256 _wstETHAmount) external virtual returns (uint256);
+
+    function getWstETHByStETH(uint256 _stETHAmount)
+        external
+        view
+        virtual
+        returns (uint256);
+}
