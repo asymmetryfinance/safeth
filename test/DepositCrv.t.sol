c0a7439b165a5c2e477150ec83fb300dfd0f3fc9
Matt <matt.b.wilder1@gmail.com>
2022-09-13 19:35:53 -0600

    add addCrvLiquidity to strat

--- test/DepositCrv.t.sol
+++ test/DepositCrv.t.sol
@@ -3,11 +3,19 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
 import "forge-std/console.sol";
+import "../src/DepositCrv.sol";
 
 // import "../src/StrategyGoldenRatio.sol";
 
 contract DepositCrvTest is Test {
+    DepositCrv private crv = new DepositCrv();
+
     function setUp() public {}
 
-    function testDepositCrvPool() public {}
+    function testDepositCrvPool() public {
+        // send eth to contract depositing in crv pool
+        (bool sent, ) = address(crv).call{value: 16e18}("");
+        require(sent, "Failed to send Ether");
+        crv.addLiquidity();
+    }
 }
