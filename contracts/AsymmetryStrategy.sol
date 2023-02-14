dbb1ac659424548a2ad363c8d9cc310a0b7477d8
Sean Luther <elmutt@gmail.com>
2023-02-14 10:59:51 -0700

    add comment

--- contracts/AsymmetryStrategy.sol
+++ contracts/AsymmetryStrategy.sol
@@ -316,12 +316,11 @@ contract AsymmetryStrategy is ERC1155Holder, Ownable {
         address sfrxEthAddress = 0xac3E018457B222d93114458476f3E3416Abbe38F;
         address frxEthAddress = 0x5E8422345238F34275888049021821E8E08CAa1f;
         address frxEthCrvPool = 0xa1F8A6807c402E4A15ef4EBa36528A3FED24E577;
-
         IsFrxEth(sfrxEthAddress).redeem(amount, address(this), address(this));
         uint256 frxEthBalance = IERC20(frxEthAddress).balanceOf(address(this));
-
         IsFrxEth(frxEthAddress).approve(frxEthCrvPool, frxEthBalance);
-
+        // TODO figure out if we want a min receive amount and what it should be
+        // Currently set to 0. It "works" but may not be ideal long term
         ICrvEthPool(frxEthCrvPool).exchange(1, 0, frxEthBalance, 0);
     }
 
