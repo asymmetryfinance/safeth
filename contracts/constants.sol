df4a7a6dc7965cc315d71917e05aef6395e9ae80
toshiSat <10103480+toshiSat@users.noreply.github.com>
2023-02-14 14:08:10 -0700

    Split contracts

---
+++ contracts/constants.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+address constant wETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+address constant CVX = 0x4e3FBD56CD56c3e72c1403e103b45Db9da5B9D2B;
+address constant rETH = 0xae78736Cd615f374D3085123A210448E74Fc6393;
+address constant veCRV = 0x5f3b5DfEb7B28CDbD7FAba78963EE202a494e2A2;
+address constant vlCVX = 0x72a19342e8F1838460eBFCCEf09F6585e32db86E;
+address constant wstETH = 0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0;
+address constant stEthToken = 0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84;
+address constant lidoCrvPool = 0xDC24316b9AE028F1497c275EB9192a3Ea0f67022;
+address constant sfrax = 0xac3E018457B222d93114458476f3E3416Abbe38F;
