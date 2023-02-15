afed7cb280b1ef802e69f1e4672fb5f5ffe922df
Sean Luther <elmutt@gmail.com>
2023-02-15 13:10:51 -0700

    make afeth ownable

--- contracts/tokens/afETH.sol
+++ contracts/tokens/afETH.sol
@@ -2,8 +2,9 @@
 pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract afETH is ERC20 {
+contract afETH is ERC20, Ownable {
     address MINTING_CONTRACT;
 
     constructor(
@@ -11,20 +12,11 @@ contract afETH is ERC20 {
         string memory _symbol
     ) ERC20(_name, _symbol) {}
 
-    // TODO: should change by governance?
-    function initialize(address _mintingContract) public {
-        require(MINTING_CONTRACT == address(0), "Already initialized");
-        require(_mintingContract != address(0), "Need valid address");
-        MINTING_CONTRACT = _mintingContract;
-    }
-
-    function mint(address to, uint256 amount) public {
-        require(msg.sender == MINTING_CONTRACT, "Must mint from contract");
+    function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }
 
-    function burn(address from, uint256 amount) public {
-        require(msg.sender == MINTING_CONTRACT, "Must burn from contract");
+    function burn(address from, uint256 amount) public onlyOwner {
         _burn(from, amount);
     }
 }
