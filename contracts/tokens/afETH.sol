fac3797540f67c425f63c5112a597a396c951920
Sean Luther <elmutt@gmail.com>
2023-02-15 13:52:29 -0700

    additional minter permissions

--- contracts/tokens/afETH.sol
+++ contracts/tokens/afETH.sol
@@ -5,18 +5,24 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract afETH is ERC20, Ownable {
-    address MINTING_CONTRACT;
+    address public minter;
 
     constructor(
         string memory _name,
         string memory _symbol
     ) ERC20(_name, _symbol) {}
 
-    function mint(address to, uint256 amount) public onlyOwner {
+    function setMinter(address newMinter) public onlyOwner {
+        minter=newMinter;
+    }
+
+    function mint(address to, uint256 amount) public {
+        require(msg.sender == minter, "not minter");
         _mint(to, amount);
     }
 
-    function burn(address from, uint256 amount) public onlyOwner {
+    function burn(address from, uint256 amount) public {
+        require(msg.sender == minter, "not minter");
         _burn(from, amount);
     }
 }
