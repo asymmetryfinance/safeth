1da01e478c552c3ca9a2c8d9753af6b2732caa37
toshiSat <10103480+toshiSat@users.noreply.github.com>
2023-03-08 11:34:28 -0700

    Remove skip missing files

--- .solcover.js
+++ .solcover.js
@@ -1,4 +1,4 @@
 // Files to ignore for test coverage reports
 module.exports = {
-    skipFiles: ['AsymmetryStrategy/AsymmetryStrategy.sol', 'tokens/afBundle1155.sol', 'tokens/afCVX1155.sol', 'Vault.sol', 'AfStrategy/upgrade_mocks/AfStrategyV2Mock.sol']
+    skipFiles: ['AsymmetryStrategy/AsymmetryStrategy.sol', 'AfStrategy/upgrade_mocks/AfStrategyV2Mock.sol']
   };
