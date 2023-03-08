5e2a9eb3fb30f7be160e02633bdc2a0f9966ce52
toshiSat <10103480+toshiSat@users.noreply.github.com>
2023-03-08 12:02:01 -0700

    Remove test

--- .solcover.js
+++ .solcover.js
@@ -1,4 +1,4 @@
 // Files to ignore for test coverage reports
 module.exports = {
-    skipFiles: ['AsymmetryStrategy/AsymmetryStrategy.sol', 'AfStrategy/upgrade_mocks/AfStrategyV2Mock.sol']
+    skipFiles: ['contracts/AsymmetryStrategy/AsymmetryStrategy.sol', 'contracts/AfStrategy/upgrade_mocks/AfStrategyV2Mock.sol']
   };
