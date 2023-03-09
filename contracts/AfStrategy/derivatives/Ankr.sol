e7f334dcf5567194af3e4c377845da992b9e06f2
Sean Luther <elmutt@gmail.com>
2023-03-09 15:34:38 -0700

    ankr done?

--- contracts/AfStrategy/derivatives/Ankr.sol
+++ contracts/AfStrategy/derivatives/Ankr.sol
@@ -2,16 +2,17 @@
 pragma solidity ^0.8.13;
 
 import "../../interfaces/IDerivative.sol";
-import "../../interfaces/frax/IsFrxEth.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "../../interfaces/ankr/AnkrStaker.sol";
+import "../../interfaces/curve/ICrvEthPool.sol";
 import "hardhat/console.sol";
 
 contract Ankr is IDerivative, Initializable, OwnableUpgradeable {
 
     address ankrEthAddress = 0xE95A203B1a91a908F9B9CE46459d101078c2c3cb;
     address ankrStakerAddress = 0x84db6eE82b7Cf3b47E8F19270abdE5718B936670;
+    address ankrEthPool = 0xA96A65c051bF88B4095Ee1f2451C2A9d43F53Ae2;
 
     uint256 public maxSlippage;
 
@@ -32,6 +33,13 @@ contract Ankr is IDerivative, Initializable, OwnableUpgradeable {
     }
 
     function withdraw(uint256 amount) public onlyOwner {
+        uint256 ankrEthBalance = IERC20(ankrEthAddress).balanceOf(address(this));
+        IERC20(ankrEthAddress).approve(ankrEthPool, ankrEthBalance);
+
+        uint256 minOut = (((ethPerDerivative(amount) * amount) / 10 ** 18) *
+            (10 ** 18 - maxSlippage)) / 10 ** 18;
+
+        ICrvEthPool(ankrEthPool).exchange(1, 0, ankrEthBalance, minOut);
         (bool sent, ) = address(msg.sender).call{value: address(this).balance}(
             ""
         );
@@ -42,19 +50,20 @@ contract Ankr is IDerivative, Initializable, OwnableUpgradeable {
         uint256 ankrBalancePre = IERC20(ankrEthAddress).balanceOf(
             address(this)
         );
-        AnkrStaker(ankrStakerAddress).stakeAndClaimAethC{value: msg.value}(address(this));
+        AnkrStaker(ankrStakerAddress).stakeAndClaimAethC{value: msg.value}();
         uint256 ankrBalancePost = IERC20(ankrEthAddress).balanceOf(
             address(this)
         );
         return ankrBalancePost - ankrBalancePre;
     }
 
-    function ethPerDerivative() public view returns (uint256) {
-        return 0;
+    function ethPerDerivative(uint256 amount) public view returns (uint256) {
+        return ((10 ** 18 * amount) /
+            ICrvEthPool(ankrEthPool).price_oracle());
     }
 
     function totalEthValue() public view returns (uint256) {
-        return (ethPerDerivative() * balance()) / 10 ** 18;
+        return (ethPerDerivative(balance()) * balance()) / 10 ** 18;
     }
 
     function balance() public view returns (uint256) {
--- contracts/interfaces/ankr/AnkrStaker.sol
+++ contracts/interfaces/ankr/AnkrStaker.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.13;
 
 // I couldnt find an official interface so I added the functions we need here
-https://etherscan.io/address/0x37ac345fa1428e3198b6a0d71deed41d83c140d3#code
+// https://etherscan.io/address/0x37ac345fa1428e3198b6a0d71deed41d83c140d3#code
 interface AnkrStaker {
-    function stakeAndClaimAethC() public whenNotPaused("stake") unlocked(msg.sender) payable
+    function stakeAndClaimAethC() external payable;
 }
