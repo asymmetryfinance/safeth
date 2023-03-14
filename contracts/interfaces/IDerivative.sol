45a137f32be370f2e8362965bd19fa2de43eec51
Sean Luther <elmutt@gmail.com>
2023-03-14 14:01:23 -0600

    oops

--- contracts/interfaces/IDerivative.sol
+++ contracts/interfaces/IDerivative.sol
@@ -8,15 +8,15 @@ interface IDerivative {
     /// buys the underlying derivative for this contract
     function deposit() external payable returns (uint256);
 
-    // sells the underlying derivative for this contract & sends user eth
+    /// sells the underlying derivative for this contract & sends user eth
     function withdraw(uint256 amount) external;
 
-    // Estimated price per derivative when depositing amount
+    /// Estimated price per derivative when depositing amount
     function ethPerDerivative(uint256 amount) external view returns (uint256);
 
-    // underlying derivative balance held by this contract
+    /// underlying derivative balance held by this contract
     function balance() external view returns (uint256);
 
-    // Maximum acceptable slippage when buying/selling underlying derivative
+    /// Maximum acceptable slippage when buying/selling underlying derivative
     function setMaxSlippage(uint256 slippage) external;
 }
