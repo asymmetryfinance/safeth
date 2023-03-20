54ac823b32b201f00a78100f61ae2cbd02c0eed2
Sean Luther <elmutt@gmail.com>
2023-03-20 17:50:15 -0600

    fix tests latest block

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -50,7 +50,7 @@ const config: HardhatUserConfig = {
       },
       forking: {
         url: process.env.MAINNET_URL || "",
-        blockNumber: 16820726,
+        blockNumber: 16871866,
         enabled: true, // Set to false to disable forked mainnet mode
       },
     },
