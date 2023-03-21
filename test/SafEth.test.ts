a2d74647efe68899ea0f200b3b3c0c1f48009199
toshiSat <10103480+toshiSat@users.noreply.github.com>
2023-03-21 14:45:01 -0600

    Reset max amount

--- test/SafEth.test.ts
+++ test/SafEth.test.ts
@@ -53,7 +53,7 @@ describe("Af Strategy", function () {
   describe("Large Amounts", function () {
     it("Should deposit and withdraw a large amount with minimal loss from slippage", async function () {
       const startingBalance = await adminAccount.getBalance();
-      const depositAmount = ethers.utils.parseEther("500");
+      const depositAmount = ethers.utils.parseEther("200");
       const tx1 = await safEthProxy.stake({ value: depositAmount });
       const mined1 = await tx1.wait();
       const networkFee1 = mined1.gasUsed.mul(mined1.effectiveGasPrice);
