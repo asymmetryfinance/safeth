f3a8e9ff66e99aed96e758467a3ad3f8eeb39ff3
toshiSat <10103480+toshiSat@users.noreply.github.com>
2023-04-04 12:51:12 -0600

    Readd positiosn struct

--- contracts/AfEth/interfaces/IAf1155.sol
+++ contracts/AfEth/interfaces/IAf1155.sol
@@ -3,14 +3,9 @@ pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 
-interface IAfCVX1155 is IERC1155 {
+interface IAf1155 is IERC1155 {
     function mint(address recipient, uint256 id, uint256 amount) external;
 
     function burn(address from, uint256 id, uint256 amount) external;
 }
 
-interface IAfBundle1155 is IERC1155 {
-    function mint(address recipient, uint256 id, uint256 amount) external;
-
-    function burn(address from, uint256 id, uint256 amount) external;
-}
