83edea91b689fe987bc9faa34544a550e9cd554f
toshiSat <10103480+toshiSat@users.noreply.github.com>
2023-04-12 10:48:44 -0600

    add gap storage

--- contracts/SafEth/derivatives/SfrxEth.sol
+++ contracts/SafEth/derivatives/SfrxEth.sol
@@ -124,4 +124,11 @@ contract SfrxEth is IDerivative, Initializable, OwnableUpgradeable {
     }
 
     receive() external payable {}
+
+    /**
+     * @dev This empty reserved space is put in place to allow future versions to add new
+     * variables without shifting down storage in the inheritance chain.
+     * See https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps
+     */
+    uint256[50] private __gap;
 }
--- contracts/SafEth/derivatives/WstEth.sol
+++ contracts/SafEth/derivatives/WstEth.sol
@@ -95,4 +95,11 @@ contract WstEth is IDerivative, Initializable, OwnableUpgradeable {
     }
 
     receive() external payable {}
+
+    /**
+     * @dev This empty reserved space is put in place to allow future versions to add new
+     * variables without shifting down storage in the inheritance chain.
+     * See https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps
+     */
+    uint256[50] private __gap;
 }
