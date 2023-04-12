37c8de242f0e9e23214073bc3302f566ae57d2e5
toshiSat <10103480+toshiSat@users.noreply.github.com>
2023-04-12 13:20:10 -0600

    remove test functions

--- contracts/SafEth/SafEth.sol
+++ contracts/SafEth/SafEth.sol
@@ -177,26 +177,6 @@ contract SafEth is
         emit WeightChange(_derivativeIndex, _weight);
     }
 
-    function toHexDigit(uint8 d) internal pure returns (bytes1) {
-        if (0 <= d && d <= 9) {
-            return bytes1(uint8(bytes1("0")) + d);
-        } else if (10 <= uint8(d) && uint8(d) <= 15) {
-            return bytes1(uint8(bytes1("a")) + d - 10);
-        }
-        revert();
-    }
-
-    function fromCode(bytes4 code) public pure returns (string memory) {
-        bytes memory result = new bytes(10);
-        result[0] = bytes1("0");
-        result[1] = bytes1("x");
-        for (uint i = 0; i < 4; ++i) {
-            result[2 * i + 2] = toHexDigit(uint8(code[i]) / 16);
-            result[2 * i + 3] = toHexDigit(uint8(code[i]) % 16);
-        }
-        return string(result);
-    }
-
     /**
         @notice - Adds new derivative to the index fund
         @param _contractAddress - Address of the derivative contract launched by AF
