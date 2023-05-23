2f6446e73b3cc68d54876ed0719de717e6a81d14
toshiSat <10103480+toshiSat@users.noreply.github.com>
2023-05-23 14:36:47 -0600

    Update gap

--- contracts/SafEth/SafEthStorage.sol
+++ contracts/SafEth/SafEthStorage.sol
@@ -35,5 +35,5 @@ contract SafEthStorage {
      * variables without shifting down storage in the inheritance chain.
      * See https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps
      */
-    uint256[47] private __gap;
+    uint256[46] private __gap;
 }
