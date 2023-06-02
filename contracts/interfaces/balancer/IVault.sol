0789790299f4dbb4fe98a64656dc299ff02984d1
toshiSat <10103480+toshiSat@users.noreply.github.com>
2023-06-02 15:17:08 -0600

    Remove interface functions

--- contracts/interfaces/balancer/IVault.sol
+++ contracts/interfaces/balancer/IVault.sol
@@ -16,14 +16,6 @@ interface IVault {
         bytes userData;
     }
 
-    struct BatchSwapStep {
-        bytes32 poolId;
-        uint256 assetInIndex;
-        uint256 assetOutIndex;
-        uint256 amount;
-        bytes userData;
-    }
-
     struct FundManagement {
         address sender;
         bool fromInternalBalance;
@@ -35,15 +27,6 @@ interface IVault {
         SingleSwap memory singleSwap,
         FundManagement memory funds,
         uint256 limit,
-        uint256 deadline
-    ) external payable returns (uint256);
-
-    function batchSwap(
-        SwapKind kind,
-        BatchSwapStep[] memory swaps,
-        address[] memory assets,
-        FundManagement memory funds,
-        int256[] memory limits,
         uint256 deadline
     ) external payable returns (uint256);
 }
