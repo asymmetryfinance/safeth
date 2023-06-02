9a69d1f7b911582b61f52f1f4d926afa4a5bad72
toshiSat <10103480+toshiSat@users.noreply.github.com>
2023-06-02 15:31:41 -0600

    Remove comment

--- contracts/interfaces/swell/ISwellEth.sol
+++ contracts/interfaces/swell/ISwellEth.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
-// I couldnt find an official interface so I added the functions we need here
-// https://etherscan.io/address/0x3ed1dfbccf893b7d2d730ead3e5edbf1f8f95a48#code
 interface ISwellEth {
     function deposit() external payable;
 
