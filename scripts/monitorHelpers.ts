eacb0d0adf5da36e438ad5b96ecc9a3571050a51
toshiSat <10103480+toshiSat@users.noreply.github.com>
2023-06-19 10:44:23 -0600

    Remove console

--- scripts/monitorHelpers.ts
+++ scripts/monitorHelpers.ts
@@ -32,7 +32,7 @@ export const notifyOnStakeUnstake = async () => {
     console.log("previousTotalSupply is", previousTotalSupply);
     if (newTotalSupply.gt(previousTotalSupply)) {
       const events = await safEth.queryFilter("Staked", 0, "latest");
-      console.log({ events });
+
       const latestEvent = events[events.length - 1];
       if (!latestEvent) {
         console.log("Failed to get Staked event data");
