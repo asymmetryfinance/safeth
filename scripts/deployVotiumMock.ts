bd615d61fa2a00093f867d057dd8843ddee67053
Sean Luther <elmutt@gmail.com>
2023-06-19 09:52:57 -0600

    add files

---
+++ scripts/deployVotiumMock.ts
@@ -0,0 +1,17 @@
+import { upgrades, ethers } from "hardhat";
+
+async function main() {
+  const VotiumMockFactory = await ethers.getContractFactory("VotiumMock");
+  const votiumMock = await upgrades.deployProxy(VotiumMockFactory, []);
+
+  await votiumMock.deployed();
+
+  console.log("Votium Mock deployed to:", votiumMock.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
