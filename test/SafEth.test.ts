8d28713ddbc9c5dc96753da92f083acb4e1f85b1
Sean Luther <elmutt@gmail.com>
2023-06-19 12:23:45 -0600

    fix

--- test/SafEth.test.ts
+++ test/SafEth.test.ts
@@ -22,7 +22,7 @@ import ERC20 from "@openzeppelin/contracts/build/contracts/ERC20.json";
 import { getUserAccounts } from "./helpers/integrationHelpers";
 import { within1Percent } from "./helpers/functions";
 
-describe.only("SafEth", function () {
+describe("SafEth", function () {
   let adminAccount: SignerWithAddress;
   let safEth: SafEth;
   let safEthReentrancyTest: SafEthReentrancyTest;
