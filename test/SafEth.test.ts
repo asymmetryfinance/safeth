01bae899726c2308cca2fdfdc6d82c6b0a27363f
toshiSat <10103480+toshiSat@users.noreply.github.com>
2023-07-06 15:33:19 -0600

    Use each derivative

--- test/SafEth.test.ts
+++ test/SafEth.test.ts
@@ -1676,20 +1676,33 @@ describe("SafEth", function () {
   describe.only("Upgrade", () => {
     it("Should upgrade mainnet SafEth, Lido, Reth, and sfrxEth with new implementations", async () => {
       const contracts = [
-        "0x6732Efaf6f39926346BeF8b821a04B6361C4F3e5",
-        "0x972A53e3A9114f61b98921Fb5B86C517e8F23Fad",
-        "0x36Ce17a5c81E74dC111547f5DFFbf40b8BF6B20A",
-        "0x7B6633c0cD81dC338688A528c0A3f346561F5cA3",
+        {
+          contractAddress: "0x6732Efaf6f39926346BeF8b821a04B6361C4F3e5",
+          factory: "SafEth",
+        },
+        {
+          contractAddress: "0x972A53e3A9114f61b98921Fb5B86C517e8F23Fad",
+          factory: "WstEth",
+        },
+        {
+          contractAddress: "0x36Ce17a5c81E74dC111547f5DFFbf40b8BF6B20A",
+          factory: "SfrxEth",
+        },
+        {
+          contractAddress: "0x7B6633c0cD81dC338688A528c0A3f346561F5cA3",
+          factory: "Reth",
+        },
       ];
       for (let i = 0; i < contracts.length; i++) {
-        const contractAddress = contracts[i];
+        const contractAddress = contracts[i].contractAddress;
+        const factory = contracts[i].factory;
         const adminAddress = "0x263b03BbA0BbbC320928B6026f5eAAFAD9F1ddeb";
         const proxyAdminContract = new ethers.Contract(
           "0x2b408df4B4e112276119bd6Ca1710b6C9966809E",
           proxyAdminAbi,
           adminAccount
         );
-        const SafEthFactory = await ethers.getContractFactory("SafEth");
+        const Factory = await ethers.getContractFactory(factory);
 
         await network.provider.request({
           method: "hardhat_impersonateAccount",
@@ -1700,10 +1713,10 @@ describe("SafEth", function () {
         const implementationBefore = await proxyAdmin.getProxyImplementation(
           contractAddress
         );
-        await upgrades.forceImport(contractAddress, SafEthFactory); // I think this will affect "prepareUpgrade"
+        await upgrades.forceImport(contractAddress, Factory); // I think this will affect "prepareUpgrade"
         const upgradeAddress = await upgrades.prepareUpgrade(
           contractAddress,
-          SafEthFactory
+          Factory
         );
         await proxyAdmin.upgrade(contractAddress, upgradeAddress);
         const implementationAfter = await proxyAdmin.getProxyImplementation(
