83c48dc776b751defe00d18b436f3a996fa75448
toshiSat <10103480+toshiSat@users.noreply.github.com>
2023-08-02 21:00:15 +0100

    v1.0.6

--- scripts/upgradePremint.ts
+++ scripts/upgradePremint.ts
@@ -10,3 +10,10 @@ async function main() {
 
   console.log("SafEth proposal at: ", safEthProposal.url);
 }
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
