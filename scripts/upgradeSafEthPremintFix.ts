ca7fcfa982346b91ae3c8a56bd85ad14afe35b4e
toshiSat <10103480+toshiSat@users.noreply.github.com>
2023-08-14 11:29:16 -0600

    v1.0.7

---
+++ scripts/upgradeSafEthPremintFix.ts
@@ -0,0 +1,18 @@
+import { ethers, defender } from "hardhat";
+
+async function main() {
+  const SafEthDeployment = await ethers.getContractFactory("SafEth");
+
+  const safEthProposal = await defender.proposeUpgrade(
+    "0x6732Efaf6f39926346BeF8b821a04B6361C4F3e5",
+    SafEthDeployment
+  );
+  console.log("SafEth proposal at: ", safEthProposal.url);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
